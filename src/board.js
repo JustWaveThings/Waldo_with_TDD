2d7bf41be15881352454dd1b289a7d5ecb5cd41a
JustWaveThings <justwavethings@proton.me>
2023-07-01 18:05:05 -0400

    Test Board

---
+++ src/board.js
@@ -0,0 +1,5 @@
+export default function Board(characters) {
+  const example = null;
+  const characterArray = [...characters];
+  return { example, characterArray };
+}
---
+++ src/board.test.js
@@ -0,0 +1,18 @@
+import Board from './board';
+
+describe('Testing Board', () => {
+  let testBoard;
+  const testCharacters = [{ a: false }, { b: false }, { c: false }];
+  beforeAll(() => {
+    testBoard = Board(testCharacters);
+  });
+
+  describe('Board exists', () => {
+    test('should see if it exists', () => {
+      expect(testBoard).toBeDefined();
+    });
+    test('initialized with characters', () => {
+      expect(testBoard.characterArray).toEqual(testCharacters);
+    });
+  });
+});
