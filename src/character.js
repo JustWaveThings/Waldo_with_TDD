5429b442a9f6a4541c9f646eb193835b0d9340c0
JustWaveThings <justwavethings@proton.me>
2023-07-10 16:02:31 -0400

    Update character.js and tests. All tests pass.

--- src/character.js
+++ src/character.js
@@ -1,13 +1,27 @@
+/* eslint-disable no-return-assign */
 const Character = (name, centerXY, radiusLength) => {
+  let found = false;
+
+  const updateFoundStatus = () => !found && (found = true);
   const checkFound = queryCoords => {
     const [centerX, centerY] = centerXY;
     const [queryX, queryY] = queryCoords;
 
     const distance = Math.sqrt((queryX - centerX) ** 2 + (queryY - centerY) ** 2);
+    const isFound = distance <= radiusLength;
 
-    return distance <= radiusLength;
+    if (isFound) {
+      updateFoundStatus();
+    }
+
+    return isFound;
   };
-  return { name, checkFound };
+
+  const getCheckFound = coord => checkFound(coord);
+  const charObject = { pubName: () => name, pubFound: () => found };
+
+  // public properties / methods
+  return { charObject, getCheckFound };
 };
 
 export default Character;
--- src/character.test.js
+++ src/character.test.js
@@ -1,15 +1,25 @@
 import Character from './character';
 
 describe('Character', () => {
-  describe('name', () => {
+  describe('charObject', () => {
     test('should return the name of the character', () => {
       // arrange
       const Bob = Character('Bob');
+      const arrange = Bob.charObject.pubName();
       const assert = 'Bob';
       // act
-      // ??
+
       // assert
-      expect(Bob.name).toBe(assert);
+      expect(arrange).toBe(assert);
+    });
+    test('should return value of the found property', () => {
+      // arrange
+      const Bob = Character('Bob');
+      const arrange = Bob.charObject.pubFound();
+      const assert = false;
+      // act
+      // assert
+      expect(arrange).toBe(assert);
     });
   });
 
@@ -21,7 +31,7 @@ describe('Character', () => {
       // act
       const act = [3, 4];
       // assert
-      expect(Bob.checkFound(act)).toBe(assert);
+      expect(Bob.getCheckFound(act)).toBe(assert);
     });
     test('should return true if query coordinates are on the edge of the circle', () => {
       // arrange
@@ -30,7 +40,7 @@ describe('Character', () => {
       // act
       const act = [5, 0];
       // assert
-      expect(Bob.checkFound(act)).toBe(assert);
+      expect(Bob.getCheckFound(act)).toBe(assert);
     });
 
     test('should return false if query coordinates are outside the circle', () => {
@@ -40,8 +50,32 @@ describe('Character', () => {
       // act
       const act = [7, 8];
       // assert
+      expect(Bob.getCheckFound(act)).toBe(assert);
+    });
 
-      expect(Bob.checkFound(act)).toBe(assert);
+    test('should return true on second call if query coordinates are within the circle', () => {
+      // arrange
+      const Bob = Character('Bob', [0, 0], 5);
+      const assert = true;
+      // act
+      const act = [3, 4];
+      Bob.getCheckFound(act);
+      Bob.getCheckFound(act);
+      // assert
+      expect(Bob.charObject.pubFound()).toBe(assert);
+    });
+
+    test('should return true after all calls if query coordinates on any of the calls are within the circle', () => {
+      // arrange
+      const Bob = Character('Bob', [0, 0], 5);
+      const assert = true;
+      // act
+      const act = [3, 4];
+      Bob.getCheckFound(act);
+      Bob.getCheckFound(act);
+      Bob.getCheckFound([15, 15]); // outside the circle to test it didn't flip back to false
+      // assert
+      expect(Bob.charObject.pubFound()).toBe(assert);
     });
   });
 });
