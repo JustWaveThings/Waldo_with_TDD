54ca3caefd7d3e2bbf287bbb01bda3e1a064a2f1
JustWaveThings <justwavethings@proton.me>
2023-07-10 13:11:55 -0400

    Add AAA to tests

--- src/character.test.js
+++ src/character.test.js
@@ -2,25 +2,42 @@ import Character from './character';
 
 describe('Character', () => {
   describe('name', () => {
-    it('should return the name of the character', () => {
+    test('should return the name of the character', () => {
+      // arrange
       const Bob = Character('Bob');
-      expect(Bob.name).toBe('Bob');
+      const assert = 'Bob';
+      // act
+      // assert
+      expect(Bob.name).toBe(assert);
     });
   });
 
   describe('checkFound', () => {
-    it('should return true if query coordinates are within the circle', () => {
+    test('should return true if query coordinates are within the circle', () => {
+      // arrange
       const Bob = Character('Bob', [0, 0], 5);
-      expect(Bob.checkFound([3, 4])).toBe(true);
+      const assert = true;
+      // act
+      // assert
+      expect(Bob.checkFound([3, 4])).toBe(assert);
     });
-    it('should return true if query coordinates are on the edge of the circle', () => {
+    test('should return true if query coordinates are on the edge of the circle', () => {
+      // arrange
       const Bob = Character('Bob', [0, 0], 5);
-      expect(Bob.checkFound([5, 0])).toBe(true);
+      const assert = true;
+      // act
+      // assert
+      expect(Bob.checkFound([5, 0])).toBe(assert);
     });
 
-    it('should return false if query coordinates are outside the circle', () => {
+    test('should return false if query coordinates are outside the circle', () => {
+      // arrange
       const Bob = Character('Bob', [0, 0], 5);
-      expect(Bob.checkFound([7, 8])).toBe(false);
+      const assert = false;
+      // act
+      // assert
+
+      expect(Bob.checkFound([7, 8])).toBe(assert);
     });
   });
 });
